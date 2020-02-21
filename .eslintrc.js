424648e4462d43865939f12b86c412815f55e2eb
Simon Warta <simon@warta.it>
2020-02-21 15:07:02 +0100

    Add linter settings

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "simple-import-sort"],
+  extends: [
+    "eslint:recommended",
+    "react-app",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "no-empty": "off",
+    "no-console": [
+      "warn",
+      {
+        allow: ["error", "info", "warn"],
+      },
+    ],
+    "no-param-reassign": "warn",
+    "prefer-const": "warn",
+    "sort-imports": "off", // we use the simple-import-sort plugin instead
+    "spaced-comment": ["warn", "always", { line: { markers: ["/ <reference"] } }],
+    "simple-import-sort/sort": "warn",
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        allowExpressions: true,
+      },
+    ],
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+  },
+  overrides: [
+    {
+      files: "**/*.js",
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React from "react";
+
+import logo from "./logo.svg";
+
+interface Codes {
+  readonly ids: number[];
+}
+
+const CodesContext = React.createContext<Codes>({ ids: [] });
+
+function App(): JSX.Element {
+  const codes: Codes = { ids: [] };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CodesContext.Provider value={codes}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </CodesContext.Provider>
   );
 }
 
