fd5fda88282f765bf52a06455b49fe58f0690f51
Simon Warta <simon@warta.it>
2020-02-28 13:22:32 +0100

    Display full verification details in code details

--- src/code/CodePage.tsx
+++ src/code/CodePage.tsx
@@ -1,55 +1,66 @@
 import "./CodeDetails.css";
 
-import { Contract, CosmWasmClient } from "@cosmwasm/sdk";
+import { CodeDetails, Contract, CosmWasmClient } from "@cosmwasm/sdk";
 import React from "react";
 import { useParams } from "react-router-dom";
 
 import { settings } from "../settings";
+import Verification from "./Verfication";
 
 function CodePage(): JSX.Element {
   const { codeId: codeIdParam } = useParams();
   const codeId = parseInt(codeIdParam || "0", 10);
 
-  const [size, setSize] = React.useState<number | undefined>();
+  const [details, setDetails] = React.useState<CodeDetails | undefined>();
   const [contracts, setContracts] = React.useState<readonly Contract[]>([]);
 
   React.useEffect(() => {
     const client = new CosmWasmClient(settings.nodeUrl);
     client.getContracts(codeId).then(setContracts);
-    client.getCodeDetails(codeId).then(code => {
-      setSize(code.wasm.length);
-    });
+    client.getCodeDetails(codeId).then(setDetails);
   }, [codeId]);
 
   return (
-    <div className="container">
-      <div className="code-details">
-        <h1>Code #{codeId}</h1>
-        <ul className="list-group list-group-horizontal">
-          <li className="list-group-item">Type: Wasm</li>
-          <li className="list-group-item">Size: {size ? Math.round(size / 1024) + " KiB" : "Loading …"}</li>
-        </ul>
-        <h2>Instances</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Label</th>
-              <th scope="col">Address</th>
-              <th scope="col">Creator</th>
-            </tr>
-          </thead>
-          <tbody>
-            {contracts.map((contract, index) => (
+    <div className="container code-details">
+      <div className="row">
+        <div className="col">
+          <h1>Code #{codeId}</h1>
+          <ul className="list-group list-group-horizontal">
+            <li className="list-group-item">Type: Wasm</li>
+            <li className="list-group-item">
+              Size: {details ? Math.round(details.data.length / 1024) + " KiB" : "Loading …"}
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <h2>Verification</h2>
+          {details ? <Verification code={details} /> : <p>Loading …</p>}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <h2>Instances</h2>
+          <table className="table">
+            <thead>
               <tr>
-                <th scope="row">{index + 1}</th>
-                <td>{contract.label}</td>
-                <td>{contract.address}</td>
-                <td>{contract.creator}</td>
+                <th scope="col">#</th>
+                <th scope="col">Label</th>
+                <th scope="col">Address</th>
+                <th scope="col">Creator</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {contracts.map((contract, index) => (
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>{contract.label}</td>
+                  <td>{contract.address}</td>
+                  <td>{contract.creator}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
---
+++ src/code/VerifyContract.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Props {
+  readonly checksum: string;
+  readonly source: string | undefined;
+  readonly builder: string | undefined;
+}
+
+function VerifyContract({ checksum, source, builder }: Props): JSX.Element {
+  if (!source || !builder)
+    return (
+      <span>
+        Contract cannot be verified since <code>builder</code> or <code>source</code> is missing
+      </span>
+    );
+
+  return (
+    <code>
+      cosmwasm-verify "{source}" "{builder}" {checksum}
+    </code>
+  );
+}
+
+export default VerifyContract;
