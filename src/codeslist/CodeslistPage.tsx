0ce5c23c950896f3fa55a81703401f4b7c127efa
Simon Warta <simon@warta.it>
2020-02-28 11:02:56 +0100

    Add routing

---
+++ src/codeslist/CodeslistPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Codes } from "./Codes";
+
+function CodeslistPage(): JSX.Element {
+  return (
+    <div className="container">
+      <Codes />
+    </div>
+  );
+}
+
+export default CodeslistPage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import App from "./App";
+import CodeslistPage from "./codeslist/CodeslistPage";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route component={() => <CodeslistPage />} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
