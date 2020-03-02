a8f6eb47ae26c68ef0fcd8d7745dd066d6e154ad
Simon Warta <simon@warta.it>
2020-03-02 16:42:23 +0100

    Fix name, docs, deployment

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# Code explorer
+# CosmWasm Code Explorer
+
+## Use hosted
+
+The explorer is deployed at https://confio.github.io/code-explorer and is configured
+for Demonet.
+
+## Run locally
+
+The node URL is configured in `src/settings.ts` and defaults to `http://localhost:1317` for development builds.
+
+```sh
+yarn install
+yarn start
+```
 
 ## Credits
 
--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CodePage from "./code/CodePage";
 import CodeslistPage from "./codeslist/CodeslistPage";
 import ContractPage from "./contract/ContractPage";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -20,8 +19,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root"),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
