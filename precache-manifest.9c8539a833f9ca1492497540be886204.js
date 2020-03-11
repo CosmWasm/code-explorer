c1ba76b40f963d503506b507c704e2acdc364746
Simon Warta <simon@warta.it>
2020-03-11 17:31:48 +0100

    Updates

---
+++ precache-manifest.9c8539a833f9ca1492497540be886204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15087c47a615fe27993d49f9af0e51ed",
+    "url": "https://cosmwasm.github.io/code-explorer/index.html"
+  },
+  {
+    "revision": "485dd37535d916e786cd",
+    "url": "https://cosmwasm.github.io/code-explorer/static/css/2.3380a8d2.chunk.css"
+  },
+  {
+    "revision": "f122f52aa921ddd0ab81",
+    "url": "https://cosmwasm.github.io/code-explorer/static/css/main.fad41145.chunk.css"
+  },
+  {
+    "revision": "485dd37535d916e786cd",
+    "url": "https://cosmwasm.github.io/code-explorer/static/js/2.9ec96cfb.chunk.js"
+  },
+  {
+    "revision": "24fe9b60472ce2d662c75de7f1ab079f",
+    "url": "https://cosmwasm.github.io/code-explorer/static/js/2.9ec96cfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f122f52aa921ddd0ab81",
+    "url": "https://cosmwasm.github.io/code-explorer/static/js/main.f5c9de3b.chunk.js"
+  },
+  {
+    "revision": "e85a61dc8af59228b539",
+    "url": "https://cosmwasm.github.io/code-explorer/static/js/runtime-main.4fe8d0f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://confio.github.io/code-explorer/precache-manifest.98bb62e2d289a0624191988c5cb84f59.js"
+  "https://cosmwasm.github.io/code-explorer/precache-manifest.9c8539a833f9ca1492497540be886204.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://confio.github.io/code-explorer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://cosmwasm.github.io/code-explorer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
