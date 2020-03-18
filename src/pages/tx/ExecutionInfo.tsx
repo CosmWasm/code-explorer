971ff94554cbe0ee33c1a5399140fe9c00e2d068
Simon Warta <simon@warta.it>
2020-03-18 16:20:42 +0100

    Show execution success of tx

--- src/pages/tx/ExecutionInfo.tsx
+++ src/pages/tx/ExecutionInfo.tsx
@@ -2,17 +2,27 @@ import { IndexedTx } from "@cosmwasm/sdk";
 import { Encoding } from "@iov/encoding";
 import React from "react";
 
+const checkMark = "✔"; // U+2714 HEAVY CHECK MARK
+const xMark = "🗙"; // U+1F5D9 CANCELLATION X
+
 interface Props {
   readonly tx: IndexedTx;
 }
 
 export function ExecutionInfo({ tx }: Props): JSX.Element {
   const time = Encoding.fromRfc3339(tx.timestamp);
+  const success = tx.code === 0;
+
   return (
     <ul className="list-group list-group-horizontal mb-3">
       <li className="list-group-item">Height: {tx.height}</li>
       <li className="list-group-item">
         <span title={tx.timestamp}>Time: {time.toLocaleString()}</span>
+      </li>
+      <li className="list-group-item">
+        <span title={`Execution succeeded: ${success ? "yes" : "no"}`}>
+          Success: {success ? checkMark : `${xMark} (error code ${tx.code})`}
+        </span>
       </li>
     </ul>
   );
