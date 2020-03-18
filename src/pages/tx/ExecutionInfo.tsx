861e862b3519f2faa9eac307a2f02fdc309adc5e
Simon Warta <simon@warta.it>
2020-03-18 15:44:47 +0100

    Localize time stamp

--- src/pages/tx/ExecutionInfo.tsx
+++ src/pages/tx/ExecutionInfo.tsx
@@ -1,4 +1,5 @@
 import { IndexedTx } from "@cosmwasm/sdk";
+import { Encoding } from "@iov/encoding";
 import React from "react";
 
 interface Props {
@@ -6,10 +7,13 @@ interface Props {
 }
 
 export function ExecutionInfo({ tx }: Props): JSX.Element {
+  const time = Encoding.fromRfc3339(tx.timestamp);
   return (
     <ul className="list-group list-group-horizontal mb-3">
       <li className="list-group-item">Height: {tx.height}</li>
-      <li className="list-group-item">Time: {tx.timestamp}</li>
+      <li className="list-group-item">
+        <span title={tx.timestamp}>Time: {time.toLocaleString()}</span>
+      </li>
     </ul>
   );
 }
