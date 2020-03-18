77729e48f86c5719776ca9737e8058d8a861bcdd
Simon Warta <simon@warta.it>
2020-03-18 16:39:11 +0100

    Add getFileType

---
+++ src/pages/tx/msgs/magic.spec.ts
@@ -0,0 +1,22 @@
+import { Encoding } from "@iov/encoding";
+
+import { getFileType } from "./magic";
+
+const { fromHex } = Encoding;
+
+describe("getFileType", () => {
+  it("works", () => {
+    expect(getFileType(fromHex(""))).toBeUndefined();
+    expect(getFileType(fromHex("1F"))).toBeUndefined();
+    expect(getFileType(fromHex("1F8B"))).toEqual("gzip");
+    expect(getFileType(fromHex("1F8Baa"))).toEqual("gzip");
+    expect(getFileType(fromHex("1F8Baabb"))).toEqual("gzip");
+
+    expect(getFileType(fromHex("00"))).toBeUndefined();
+    expect(getFileType(fromHex("0061"))).toBeUndefined();
+    expect(getFileType(fromHex("006173"))).toBeUndefined();
+    expect(getFileType(fromHex("0061736d"))).toEqual("wasm");
+    expect(getFileType(fromHex("0061736daa"))).toEqual("wasm");
+    expect(getFileType(fromHex("0061736daabb"))).toEqual("wasm");
+  });
+});
---
+++ src/pages/tx/msgs/magic.ts
@@ -0,0 +1,24 @@
+import { Encoding } from "@iov/encoding";
+
+function arrayEqual(a: Uint8Array, b: Uint8Array): boolean {
+  if (a.length !== b.length) return false;
+  const difference = a.some((byte, index) => b[index] !== byte);
+  return !difference;
+}
+
+function arrayStartsWith(a: Uint8Array, prefix: Uint8Array): boolean {
+  return arrayEqual(a.slice(0, prefix.length), prefix);
+}
+
+const magic = {
+  gzip: Encoding.fromHex("1F8B"),
+  wasm: Encoding.fromHex("0061736d"),
+};
+
+export type SupportedTypes = "gzip" | "wasm";
+
+export function getFileType(data: Uint8Array): SupportedTypes | undefined {
+  if (arrayStartsWith(data, magic.gzip)) return "gzip";
+  if (arrayStartsWith(data, magic.wasm)) return "wasm";
+  return undefined;
+}
