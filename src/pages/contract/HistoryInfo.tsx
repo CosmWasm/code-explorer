e7f75953a345f27557b9a6d5ca20bad191cb07bb
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-10-06 11:25:17 +0200

    Add HistoryInfo component

---
+++ src/pages/contract/HistoryInfo.tsx
@@ -0,0 +1,28 @@
+import { ContractCodeHistoryEntry } from "@cosmjs/cosmwasm";
+import React from "react";
+
+import { CodeLink } from "../../components/CodeLink";
+
+interface Props {
+  readonly contractCodeHistory: readonly ContractCodeHistoryEntry[];
+}
+
+export function HistoryInfo({ contractCodeHistory }: Props): JSX.Element {
+  return (
+    <div className="card mb-3">
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">
+          <span title="The contract level message history">History</span>
+        </li>
+        {contractCodeHistory.map((entry) => (
+          <li className="list-group-item">
+            <span title="The message operation type">
+              {entry.operation} - <CodeLink codeId={entry.codeId} />
+            </span>
+            <pre className="mb-0">{JSON.stringify(entry.msg, null, "  ")}</pre>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
