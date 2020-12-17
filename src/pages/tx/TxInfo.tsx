7d8805d4dc4dea814b6602bed1c5231d38c71bc9
willclarktech <willclarktech@users.noreply.github.com>
2020-12-17 14:53:25 +0000

    Update TxInfo for Stargate

--- src/pages/tx/TxInfo.tsx
+++ src/pages/tx/TxInfo.tsx
@@ -1,20 +1,22 @@
-import { CosmosSdkTx } from "@cosmjs/launchpad";
+import { codec } from "@cosmjs/stargate";
 import React from "react";
 
 import { printableBalance } from "../../ui-utils";
 
+type ITx = codec.cosmos.tx.v1beta1.ITx;
+
 interface Props {
-  readonly tx: CosmosSdkTx;
+  readonly tx: ITx;
 }
 
 export function TxInfo({ tx }: Props): JSX.Element {
   return (
     <div className="card mb-3">
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">Memo: {tx.value.memo || "–"}</li>
-        <li className="list-group-item">Fee: {printableBalance(tx.value.fee.amount)}</li>
-        <li className="list-group-item">Gas: {tx.value.fee.gas}</li>
-        <li className="list-group-item">Signatures: {tx.value.signatures.length}</li>
+        <li className="list-group-item">Memo: {tx.body?.memo || "–"}</li>
+        <li className="list-group-item">Fee: {printableBalance(tx.authInfo?.fee?.amount ?? [])}</li>
+        <li className="list-group-item">Gas: {tx.authInfo?.fee?.gasLimit?.toString() ?? "0"}</li>
+        <li className="list-group-item">Signatures: {tx.signatures?.length ?? 0}</li>
       </ul>
     </div>
   );
