83c79a82525bb5f83a19fecb90eba968b449087f
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-12-21 12:29:44 +0100

    Extract JSONInput style

---
+++ src/ui-utils/jsonInput.ts
@@ -0,0 +1,6 @@
+// Place error box below text box, so appearing error does not push text box down
+export const jsonInputStyle = {
+  container: { display: "flex", flexDirection: "column" },
+  body: { order: "1" },
+  warningBox: { order: "2" },
+};
