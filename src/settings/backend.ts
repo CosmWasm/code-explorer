1b27781505becd7ff59cacbf6b2714ada3e82d55
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-12-21 12:29:05 +0100

    Add addressPrefix to settings

--- src/settings/backend.ts
+++ src/settings/backend.ts
@@ -4,36 +4,42 @@ export interface BackendSettings {
   readonly nodeUrls: NonEmptyArray<string>;
   readonly stargateEnabled: boolean;
   readonly denominations: readonly string[];
+  readonly addressPrefix: string;
 }
 
 const devnetStargateSettings: BackendSettings = {
   nodeUrls: ["http://localhost:26659"],
   stargateEnabled: true,
   denominations: ["ucosm", "ustake"],
+  addressPrefix: "cosmos",
 };
 
 const devnetLaunchpadSettings: BackendSettings = {
   nodeUrls: ["http://localhost:1317"],
   stargateEnabled: false,
   denominations: ["ucosm", "ustake"],
+  addressPrefix: "cosmos",
 };
 
 const coralnetSettings: BackendSettings = {
   nodeUrls: ["https://lcd.coralnet.cosmwasm.com"],
   stargateEnabled: false,
   denominations: ["ucosm", "ustake"],
+  addressPrefix: "coral",
 };
 
 const heldernetSettings: BackendSettings = {
   nodeUrls: ["https://lcd.heldernet.cosmwasm.com"],
   stargateEnabled: false,
   denominations: ["ucosm", "ustake"],
+  addressPrefix: "cosmos",
 };
 
 const regenSettings: BackendSettings = {
   nodeUrls: ["https://regen-lcd.vitwit.com", "https://regen-relay.01node.com"],
   stargateEnabled: false,
   denominations: ["ucosm", "ustake"],
+  addressPrefix: "cosmos",
 };
 
 const knownBackends: { [index: string]: BackendSettings } = {
