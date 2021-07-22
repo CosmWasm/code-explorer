fdfb329f6541e8816bff343a4ce1f128b935df10
Giancarlos Salas <giansalex@gmail.com>
2021-07-22 15:50:01 -0500

    Parse execute contract msg

--- src/pages/tx/msgs/MsgExecuteContract.tsx
+++ src/pages/tx/msgs/MsgExecuteContract.tsx
@@ -12,6 +12,15 @@ interface Props {
 }
 
 export function MsgExecuteContract({ msg }: Props): JSX.Element {
+  const printableMsgContract = (msg: Uint8Array) => {
+    let result = '';
+    msg?.forEach((item) => {
+      result += String.fromCharCode(item);
+    });
+
+    return result;
+  };
+
   return (
     <Fragment>
       <li className="list-group-item">
@@ -20,10 +29,10 @@ export function MsgExecuteContract({ msg }: Props): JSX.Element {
       <li className="list-group-item">
         Sender: <AccountLink address={msg.sender ?? "-"} maxLength={null} />
       </li>
-      <li className="list-group-item">Sent funds: {printableBalance(msg.funds ?? [])}</li>
+      <li className="list-group-item">Sent funds: {printableBalance(msg.funds)}</li>
       <li className="list-group-item">
-        <span title="The contract level message">Handle message</span>:{" "}
-        <pre className="mb-0">{JSON.stringify(msg.msg, null, "  ")}</pre>
+        <span title="The contract level message">Handle message</span>: <br />
+        <code>{printableMsgContract(msg.msg)}</code>
       </li>
     </Fragment>
   );
