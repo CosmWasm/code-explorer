0d829ba6f1053cd8288bb4de5bdee9fc895b758a
Giancarlos Salas <giansalex@gmail.com>
2021-07-30 20:05:59 -0500

    Add newCode page

--- src/pages/code/NewCodePage.tsx
+++ src/pages/code/NewCodePage.tsx
@@ -1,11 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { ClientContext } from "../../contexts/ClientContext";
 import { FooterRow } from "../../components/FooterRow";
 import { Header } from "../../components/Header";
+import { UploadResult } from "@cosmjs/cosmwasm-stargate";
+import { Result } from "../contract/ContractPage";
 
 export function NewCodePage(): JSX.Element {
-  const pageTitle = <span>New Code</span>;
+  const { userAddress, signingClient } = React.useContext(ClientContext);
+  const [wasm, setWasm] = React.useState<File|null>();
+  const [memo, setMemo] = React.useState<string>();
+  const [source, setSource] = React.useState<string>();
+  const [builder, setBuilder] = React.useState<string>();
+  
+  const [executing, setExecuting] = React.useState(false);
+  const [executeResponse, setExecuteResponse] = React.useState<Result<UploadResult>>();
+  const [error, setError] = React.useState<string>();
+
+  React.useEffect(() => {
+    if (executeResponse?.error) {
+      setError(executeResponse.error);
+      return;
+    }
+
+    setError(undefined);
+  }, [executeResponse]);
+
+  async function uploadCode(): Promise<void> {
+    if (!userAddress || !wasm || !signingClient) return;
+
+    setExecuting(true);
+    const wasmBytes = new Uint8Array(await wasm.arrayBuffer());
+    try {
+        
+      const executeResponseResult: UploadResult = await signingClient.upload(
+        userAddress,
+        wasmBytes,
+        {
+            builder: builder,
+            source: source,
+        },
+        memo
+      );
+      setExecuteResponse({ result: executeResponseResult });
+    } catch (error) {
+      setExecuteResponse({ error: `Execute error: ${error.message}` });
+    }
+
+    setExecuting(false);
+  }
 
   return (
     <div className="page">
@@ -19,16 +63,82 @@ export function NewCodePage(): JSX.Element {
                     <Link to="/codes">Codes</Link>
                 </li>
                 <li className="breadcrumb-item active" aria-current="page">
-                    {pageTitle}
+                New Code
                 </li>
               </ol>
             </nav>
           </div>
         </div>
         <div className="row white-row white-row-last">
-          <div className="col">
-            create
-          </div>
+            <div className="col">
+                <div className="card mb-3">
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item d-flex align-items-baseline">
+                            <span title="The contract query input">Wasm:</span>
+                            <input
+                                type="file"
+                                className="ml-3 flex-grow-1"
+                                onChange={(e) => setWasm(e.target.files?.item(0))}
+                            />
+                        </li>
+                        <li className="list-group-item d-flex align-items-baseline">
+                            <span title="The contract query input">Source:</span>
+                            <input
+                                className="ml-3 flex-grow-1"
+                                value={source}
+                                onChange={(event) => setSource(event.target.value)}
+                            />
+                        </li>
+                        <li className="list-group-item d-flex align-items-baseline">
+                            <span title="The contract query input">Builder:</span>
+                            <input
+                                className="ml-3 flex-grow-1"
+                                value={builder}
+                                onChange={(event) => setBuilder(event.target.value)}
+                            />
+                        </li>
+                        <li className="list-group-item d-flex align-items-baseline">
+                            <span title="The contract query input">Memo:</span>
+                            <input
+                                className="ml-3 flex-grow-1"
+                                value={memo}
+                                onChange={(event) => setMemo(event.target.value)}
+                            />
+                        </li>
+                        <li>
+                            <div className="list-group-item btn-group">
+                                {executing ? (
+                                    <button className="btn btn-primary" type="button" disabled>
+                                        <span className="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>
+                                        Executing...
+                                    </button>
+                                ) : (
+                                    <button
+                                        className="btn btn-primary"
+                                        onClick={uploadCode}
+                                        disabled={!signingClient}
+                                    >
+                                        Upload
+                                    </button>
+                                )}
+                            </div>
+                        </li>
+                        {executeResponse?.result ? (
+                            <li className="list-group-item">
+                                <span title="The contract formatted input">Response:</span>
+                                <pre className="mb-0">{JSON.stringify(executeResponse.result, undefined, "  ")}</pre>
+                            </li>
+                        ) : null}
+                        {error ? (
+                            <li className="list-group-item">
+                                <span className="text-danger" title="The contract query error">
+                                    {error}
+                                </span>
+                            </li>
+                        ) : null}
+                    </ul>
+                </div>
+            </div>
         </div>
         <FooterRow />
       </div>
